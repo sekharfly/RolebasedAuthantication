cb92d8e019f971310f05d635f0e526e5c0993b87
sekhar <sekhar@74bbc150-5b45-4383-b7c3-26a3a4428785>
2018-02-05 05:24:22 +0000

    commited

--- e-kart3/src/main/java/com/demo/UsersApplication.java
+++ e-kart3/src/main/java/com/demo/UsersApplication.java
@@ -1,5 +1,10 @@
 package com.demo;
 
+import static javax.measure.unit.SI.KILOGRAM;
+import javax.measure.quantity.Mass;
+import org.jscience.physics.model.RelativisticModel;
+import org.jscience.physics.amount.Amount;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
