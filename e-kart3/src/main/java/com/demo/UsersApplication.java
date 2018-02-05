2715701a7ad93b78f3997d1f2508ee7b9ed09874
sekhar <sekhar@74bbc150-5b45-4383-b7c3-26a3a4428785>
2018-02-05 12:53:59 +0000

    commite

--- e-kart3/src/main/java/com/demo/UsersApplication.java
+++ e-kart3/src/main/java/com/demo/UsersApplication.java
@@ -1,12 +1,12 @@
 package com.demo;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import static javax.measure.unit.SI.KILOGRAM;
 import javax.measure.quantity.Mass;
 import org.jscience.physics.model.RelativisticModel;
 import org.jscience.physics.amount.Amount;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UsersApplication {
